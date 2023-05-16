0cb64ad7162910fe2a5b43f3d0d0616e54baca56
Caleb Heuer <ch@raiguard.me>
2023-05-16 11:20:04 -0600

    Update screenshots

--- screenshots/prepare-screenshots.sh
+++ screenshots/prepare-screenshots.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 sleep 2
-swaymsg "floating enable; resize set width 996 height 735"
+swaymsg "floating enable; resize set width 700 height 472"
--- screenshots/take-screenshots.sh
+++ screenshots/take-screenshots.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-echo "colorscheme one-darker; one-enable-fancy-underlines" | kak -p $1
+echo "colorscheme one-darker" | kak -p $1
 sleep 3
 grimshot save active ./one-darker.png
-echo "colorscheme one-dark; one-enable-fancy-underlines" | kak -p $1
+echo "colorscheme one-dark" | kak -p $1
 sleep 3
 grimshot save active ./one-dark.png
-echo "colorscheme one-light; one-enable-fancy-underlines" | kak -p $1
+echo "colorscheme one-light" | kak -p $1
 sleep 3
 grimshot save active ./one-light.png
